0e5edd92106d870f5590f75ed9ab010d76f9bc17
Jake Zweifler <jake.zweifler@gmail.com>
2022-10-09 17:59:47 -0500

    added some type translations

--- src/Elab.ml
+++ src/Elab.ml
@@ -30,9 +30,9 @@ and elab_nselmts (elmts : AbsQSharp.nSElmnt list) : AbsLambdaQs.cmd =
 and curry (params : AbsQSharp.param list) (body : AbsQSharp.body) : AbsLambdaQs.exp =
   match params with
   | [] -> failwith (unimplemented_error "Empty parameter list")
-  | [ParNI (NItem (Ident arg,typ))] ->
+  | [ParNI (NItem (UIdent arg,typ))] ->
       AbsLambdaQs.proc (MVar (Ident arg), elab_type typ, elab_body body)
-  | (ParNI (NItem (Ident arg,typ))) :: t ->
+  | (ParNI (NItem (UIdent arg,typ))) :: t ->
       AbsLambdaQs.ELam (MVar (Ident arg), elab_type typ, curry t body)
   | _ -> failwith (unimplemented_error "Nested paramss (ParNIA)")
 
@@ -41,18 +41,36 @@ and elab_calldec (calld : AbsQSharp.callDec) : AbsLambdaQs.var * AbsLambdaQs.exp
   match calld with
   | CDFun _ -> failwith (unimplemented_error "Function declarations (CDFun)")
   (* TODO: what do we want to do with characteristics? We're currently ignoring them *)
-  | CDOp (Ident name, TAEmpty, params, typ, _, body) ->
+  | CDOp (UIdent name, TAEmpty, params, typ, _, body) ->
      (match params with
       | ParTpl params ->
-          (MVar (Ident name), curry params body)
-      | _ -> failwith (unimplemented_error "Operations with multiple arguments"))
+          (MVar (Ident name), curry params body))
+      (* | _ -> failwith (unimplemented_error "Operations with multiple arguments")) *)
   | _ -> failwith (unimplemented_error "Operations with type parameters (tyArg)")
 
 and elab_type (typ : AbsQSharp.typ) : AbsLambdaQs.typ =
   match typ with
-  | TQbit -> AbsLambdaQs.TQbit (* TODO: should probably be TQref *)
+  | TEmp -> failwith (unimplemented_error "(TEmp)")
+  | TPar (TIdent tyarg) -> failwith (unimplemented_error "(TPar)")
+  | TQNm _ -> failwith (unimplemented_error "(TQNm)")
+  | TTpl typs -> failwith (unimplemented_error "(TTpl)")
+  (* is this right?? TParr is partial function, which sounds different than just the type of a function  *)
+  | TFun (ty1, ty2) -> AbsLambdaQs.TParr (elab_type ty1, elab_type ty2) 
+  (* TODO: is TOp the same type as TFun? *)
+  | TOp (ty1, ty2, chars) -> failwith (unimplemented_error "(TOp)")
+  | TArr typ -> AbsLambdaQs.TArr (elab_type typ)
+  | TBInt -> failwith (unimplemented_error "(TBInt)")
+  | TBool -> AbsLambdaQs.TBool
+  | TDbl -> failwith (unimplemented_error "(TDbl)")
+  | TInt -> AbsLambdaQs.TInt
+  | TPli -> failwith (unimplemented_error "(TPli)")
+  (* TODO: should send to Qref, but what should the key be? *)
+  | TQbit -> failwith (unimplemented_error "(TQbit)")
+  | TRng -> failwith (unimplemented_error "(TRng)")
+  | TRes -> failwith (unimplemented_error "(TRes)")
+  | TStr -> AbsLambdaQs.TStr
   | TUnit -> AbsLambdaQs.TUnit
-  | _ -> failwith (unimplemented_error "Most types (TEmp, TPar, TQNm, ...)")
+
 
 and elab_body (body : AbsQSharp.body) : AbsLambdaQs.cmd =
   match body with
@@ -65,11 +83,13 @@ and elab_stmts (stmts : AbsQSharp.stm list) : AbsLambdaQs.cmd =
   | [] -> CRet ETriv
   (* TODO: in general, we'll want to use CBnd -- what var should we bind to? *)
   | (SExp exp) :: [] -> AbsLambdaQs.CRet (elab_exp exp)
-  | _ -> failwith (unimplemented_error "Most statements (SRet, SFail, SLet, ...)")
+  | (SRet exp) :: [] -> AbsLambdaQs.CRet (elab_exp exp)
+  | _ -> failwith (unimplemented_error "Most statements (SFail, SLet, ...)")
+
 
 and elab_exp (exp : AbsQSharp.exp) : AbsLambdaQs.exp =
   match exp with
-  | EName (QUnqual (Ident x)) -> EVar (MVar (Ident x))
+  | EName (QUnqual (UIdent x)) -> EVar (MVar (Ident x))
   | ECall (e1, [e2]) -> AbsLambdaQs.EAp(elab_exp e1, elab_exp e2)
   | ECall (e1, [e2; e3]) -> AbsLambdaQs.EAp((AbsLambdaQs.EAp(elab_exp e1, elab_exp e2)), elab_exp e2)
   | _ -> failwith (unimplemented_error "Most expressions")
