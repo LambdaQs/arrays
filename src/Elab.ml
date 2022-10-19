aabd77f0178da7367601513b5a78da24ae84c67d
Kartik Singhal <kartiksinghal@gmail.com>
2022-10-19 14:05:00 -0500

    Run `pre-commit run --all-files`

--- src/Elab.ml
+++ src/Elab.ml
@@ -14,20 +14,20 @@ module Strmap = Map.Make(String)
 (* the type of an environment. TODO: figure out specifically how this works *)
 (* type env_t = { vars : (AbsLambdaQs.typ) Strmap.t } could make this a record to be nicer *)
 (* maybe check all variables and scopes etc... on LambdaQS level, but this can still be used as the stack *)
-type env_t = int Strmap.t 
+type env_t = int Strmap.t
 
 
 
 (* looks for elif* + else? + ... and returns a list of the elifs/elses, and a list of the other stuff *)
-let rec extract_ifs (stmts : AbsQSharp.stm list) : AbsQSharp.stm list * AbsQSharp.stm list = 
-  match stmts with 
-  | (SEIf (e, s)) :: stmts' -> let (ifs, stmts'') = extract_ifs stmts' 
+let rec extract_ifs (stmts : AbsQSharp.stm list) : AbsQSharp.stm list * AbsQSharp.stm list =
+  match stmts with
+  | (SEIf (e, s)) :: stmts' -> let (ifs, stmts'') = extract_ifs stmts'
                           in ((SEIf (e, s)) :: ifs, stmts'')
   | (SElse scope) :: stmts' -> ([(SElse scope)], stmts')
   | _ -> ([], stmts)
 
 
-(* should also take in signature (stack data struc to keep track of Qubits) and 
+(* should also take in signature (stack data struc to keep track of Qubits) and
    context (to keep track of variables and types) *)
 let rec elab (prog : AbsQSharp.doc) (env : env_t) : AbsLambdaQs.cmd =
   match prog with
@@ -36,7 +36,7 @@ let rec elab (prog : AbsQSharp.doc) (env : env_t) : AbsLambdaQs.cmd =
 
 and elab_nmspace (ns : AbsQSharp.nmspace) (env : env_t) : AbsLambdaQs.cmd =
   match ns with
-  (* TODO: do something with the namespace's name *) 
+  (* TODO: do something with the namespace's name *)
   (* Should probably store them in the env! *)
   | NDec (_, elmts) -> elab_nselmts elmts env
 
@@ -67,9 +67,9 @@ and curry (params : AbsQSharp.param list) (body : AbsQSharp.body) (env : env_t):
 (* what is going on here? Why these specific return values? *)
 and elab_calldec (calld : AbsQSharp.callDec) (env : env_t) : AbsLambdaQs.var * AbsLambdaQs.exp =
   match calld with
-  | CDFun (UIdent name, TAEmpty, ParTpl params, typ, body) -> 
+  | CDFun (UIdent name, TAEmpty, ParTpl params, typ, body) ->
           (MVar (Ident name), curry params body env)
-    
+
     (* TODO: what do we want to do with characteristics? We're currently ignoring them *)
   | CDOp (UIdent name, TAEmpty, ParTpl params, typ, _, body) ->
          (MVar (Ident name), curry params body env)
@@ -83,7 +83,7 @@ and elab_type (typ : AbsQSharp.typ) : AbsLambdaQs.typ =
   | TUDT _ -> failwith (unimplemented_error "(TQNm)")
   | TTpl typs -> failwith (unimplemented_error "(TTpl)")
   (* is this right?? TParr is partial function, which sounds different than just the type of a function  *)
-  | TFun (ty1, ty2) -> AbsLambdaQs.TParr (elab_type ty1, elab_type ty2) 
+  | TFun (ty1, ty2) -> AbsLambdaQs.TParr (elab_type ty1, elab_type ty2)
   (* TODO: is TOp the same type as TFun? *)
   | TOp (ty1, ty2, chars) -> failwith (unimplemented_error "(TOp)")
   | TArr typ -> AbsLambdaQs.TArr (elab_type typ)
@@ -111,7 +111,7 @@ and elab_stmts (stmts : AbsQSharp.stm list) (env : env_t) : AbsLambdaQs.cmd =
   (* TODO: shouldn't always return empty *)
   | [] -> CRet ETriv
   (* TODO: in general, we'll want to use CBnd -- what var should we bind to? *)
- 
+
   (* (* TODO: this is wrong since sometimes we want CGap *)
      | (SExp exp) :: [] -> AbsLambdaQs.CRet (elab_exp exp) *)
   (* I beleive that this is actually the correct translation: *)
@@ -119,8 +119,8 @@ and elab_stmts (stmts : AbsQSharp.stm list) (env : env_t) : AbsLambdaQs.cmd =
   (* this one is strightforward, just return the exp *)
   | (SRet exp) :: _ -> CRet (elab_exp exp) (* should things after return statement be ignored? *)
   | (SFail exp) :: stmts' -> failwith (unimplemented_error "(SFail)")
-  | (SLet (bnd, exp)) :: stmts' -> 
-    (match bnd with 
+  | (SLet (bnd, exp)) :: stmts' ->
+    (match bnd with
      | BndWild -> CBnd (wild_var, elab_exp exp, elab_stmts stmts' env)
      | BndName (UIdent var) -> CBnd (MVar (Ident var), elab_exp exp, elab_stmts stmts' env)
      | BndTplA bnds -> failwith (unimplemented_error "list binds"))
@@ -133,25 +133,25 @@ and elab_stmts (stmts : AbsQSharp.stm list) (env : env_t) : AbsLambdaQs.cmd =
   (* TODO: look up how these are done in other languages since the implementation here is probably similar *)
   (* I have some ideas for how this would work, but gets translated to exp anyways and not cmd *)
   (* will either need to figure out what VAR to bind to as in the above or do CRet (EIte)  *)
-  | (SIf (exp, scope)) :: stmts' -> 
-        let (ites, stmts'') = extract_ifs stmts' in 
+  | (SIf (exp, scope)) :: stmts' ->
+        let (ites, stmts'') = extract_ifs stmts' in
         CBnd (wild_var, elab_ite ((SIf (exp, scope)) :: ites) env, elab_stmts stmts'' env)
 
   (* these must come after if, so wont be dealt with here *)
   | (SEIf (exp, scope)) :: stmts' -> failwith ("Elif statement does not occur after an If statement")
   | (SElse scope) :: stmts' -> failwith ("Else statement does not occur after an If statement")
-  
+
   | (SFor (bnd, exp, scope)) :: stmts' -> failwith (unimplemented_error "Most statements (SFail, SLet, ...)")
   | (SWhile (exp, scope)) :: stmts' -> failwith (unimplemented_error "Most statements (SFail, SLet, ...)")
-  
+
   (* TODO: can we assume that when SUntil appears, SRep must have come before? *)
   | (SRep scope) :: stms' -> failwith (unimplemented_error "Most statements (SFail, SLet, ...)")
   | (SUntil exp) :: stms'  -> failwith (unimplemented_error "Most statements (SFail, SLet, ...)")
   | (SUntilF (exp, scope)) :: stms' -> failwith (unimplemented_error "Most statements (SFail, SLet, ...)")
-  
+
   | (SWithin scope) :: stms' -> failwith (unimplemented_error "Most statements (SFail, SLet, ...)")
   | (SApply scope) :: stms' -> failwith (unimplemented_error "Most statements (SFail, SLet, ...)")
-  
+
   | (SUse qbitBnd) :: stms' -> failwith (unimplemented_error "Most statements (SFail, SLet, ...)")
   | (SUseS (qbitBnd, scope)) :: stms' -> failwith (unimplemented_error "Most statements (SFail, SLet, ...)")
 
@@ -180,12 +180,12 @@ and elab_ite (stmts : AbsQSharp.stm list) (env : env_t) : AbsLambdaQs.exp =
         let cond' = elab_exp cond in
         let cmd1 = elab_stmts stmts' env in
         EIte (cond', ECmd cmd1, ETriv)
-  | (SIf (cond, (Scp stmts1))) :: (SElse (Scp stmts2)) :: [] -> 
+  | (SIf (cond, (Scp stmts1))) :: (SElse (Scp stmts2)) :: [] ->
         let cond' = elab_exp cond in
         let cmd1 = elab_stmts stmts1 env in
         let cmd2 = elab_stmts stmts2 env in
         EIte (cond', ECmd cmd1, ECmd cmd2)
-  | (SEIf (cond, (Scp stmts1))) :: (SElse (Scp stmts2)) :: [] -> 
+  | (SEIf (cond, (Scp stmts1))) :: (SElse (Scp stmts2)) :: [] ->
         let cond' = elab_exp cond in
         let cmd1 = elab_stmts stmts1 env in
         let cmd2 = elab_stmts stmts2 env in
@@ -195,13 +195,13 @@ and elab_ite (stmts : AbsQSharp.stm list) (env : env_t) : AbsLambdaQs.exp =
         let cmd1 = elab_stmts stmts1 env in
         let stmts'' = elab_ite stmts' env in
         EIte (cond', ECmd cmd1, stmts'')
-  | (SEIf (cond, (Scp stmts1))) :: stmts' -> 
+  | (SEIf (cond, (Scp stmts1))) :: stmts' ->
         let cond' = elab_exp cond in
         let cmd1 = elab_stmts stmts1 env in
         let stmts'' = elab_ite stmts' env in
         EIte (cond', ECmd cmd1, stmts'')
   | _ -> failwith ("Unexpected case in ITE translation")
-    
+
 
 
 
